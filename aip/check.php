f4bf0985f93577f0dc07cf0b27aa8dcfa7bcd72b
miakojason <miakojason@gmail.com>
2024-02-08 15:45:23 +0800

    平常被地都沒問題發現沒被盜add submenu

---
+++ aip/check.php
@@ -0,0 +1,6 @@
+<?php include_once "./db.php";
+if ($Admin->count(['acc' => $_POST['acc'], 'pw' => $_POST['pw']]) > 0) {
+    to("../back.php");
+} else {
+    to("../index.php?do=login&error=帳號密碼錯誤");
+}
--- aip/logout.php
+++ aip/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 unset($_SESSION['login']);
-header("../index.php");
+header("location:../index.php");
 ?>
