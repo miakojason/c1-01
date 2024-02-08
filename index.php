dbd2e25f38a6e98c4f869eeb6c2d22af7b0a9ea3
miakojason <miakojason@gmail.com>
2024-02-08 16:08:35 +0800

    新增資料表也要被練習2便感覺不一樣再接再厲，4張表跟api都背起來

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php include_once "./api/db.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -46,7 +45,8 @@
 				}
 				?>
 			<!-- main -->
-			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
+			</div>
 			<script>
 				$(".sswww").hover(
 					function() {
