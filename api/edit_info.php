e5883271de1784bcf1198fb08234686916a227bc
miakojason <miakojason@gmail.com>
2024-02-14 22:36:33 +0800

    to-17

--- api/edit_info.php
+++ api/edit_info.php
@@ -1,8 +1,7 @@
 <?php include_once "./db.php";
-$table=$_POST['table'];
-$DB=${ucfirst($table)};
-$data=$DB->find(1);
-$data[$table]=$_POST[$table];
+$table = $_POST['table'];
+$DB = ${ucfirst($table)};
+$data = $DB->find(1);
+$data[$table] = $_POST[$table];
 $DB->save($data);
 to("../back.php?do=$table");
-?>
